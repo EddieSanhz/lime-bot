253e1a16142455a646377248488b45bc44be3f09
EddieSanhz <edward_sanchez94@hotmail.com>
2021-06-13 20:31:24 -0300

    I broke some things so uh I fixed them

--- src/modules/chatbot/lib/dynamic-formatters/time.js
+++ src/modules/chatbot/lib/dynamic-formatters/time.js
@@ -4,8 +4,8 @@ module.exports = () => {
 
 	const date = new Date();
 
-	const hours = ('0' + date.getUTCHours).slice(-2);
-	const minutes = ('0' + date.getUTCMinutes).slice(-2);
+	const hours = ('0' + date.getUTCHours()).slice(-2);
+	const minutes = ('0' + date.getUTCMinutes()).slice(-2);
 
 	return `${hours}:${minutes}`;
 };
